ec4eef314be0660f9e7fc7b094e0dcd5d5dbba8f
chopster44 <oscar@chopster44.com>
2023-03-15 11:46:13 +0000

    Added Different screens

---
+++ pages/Home.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function Home() {
+	return (
+		<View>
+			<Text>Home screen</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+
+})
---
+++ pages/Login.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function Login() {
+	return (
+		<View>
+			<Text>Login screen</Text>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+
+})
