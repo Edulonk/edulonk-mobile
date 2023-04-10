37f3d3e6343655d6d2bf316fcf233764dc44b169
chopster44 <oscar@chopster44.com>
2023-04-10 16:25:03 +0100

    updated data check

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -22,7 +22,7 @@ export default function Login(props: loginProps) {
 	const [username, onSetUser] = useState("");
 	const [password, onSetPass] = useState("");
 
-	if (props.quickLog.login) {
+	if (props.quickLog.login === true) {
 		console.warn(props);
 		useEffect(()=> {
 			async function login() {
