6b326ae3ef29fa91a1144edd18175badc729e005
chopster44 <oscar@chopster44.com>
2023-04-11 15:15:31 +0100

    Removed helper script which was causing problems

--- helper/edulinkHelp.ts
+++
@@ -1,7 +0,0 @@
-import {Edulink} from "edulinkone-api";
-
-export let edulink: Edulink;
-
-export async function createEdulink(username: string, password: string, schoolId: string) {
-	edulink = new Edulink(schoolId, username, password, 2);
-}
